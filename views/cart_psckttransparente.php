c45d61012dd7b4219c33e380a68175e124145c62
Pedro Henrique <pedrook16@gmail.com>
2019-03-19 12:23:08 -0300

    bootstrap aplicado ao pagamento

--- views/cart_psckttransparente.php
+++ views/cart_psckttransparente.php
@@ -2,86 +2,100 @@
 
 <div class="row">
 	<div class="col-sm-4">
-		<h3>Dados Pessoais</h3>
+		<h3>DADOS PESSOAIS</h3>
 		<div class="form-group">
 			<label for="name">Nome:</label><br/>
-				<input type="text" class="form-control" name="name" value="Pedro Henrique" id="name"  /><br/>
+				<input type="text" class="form-control" name="name" value="Pedro Henrique" id="name"  />
 		</div>		
 		<div class="form-group">
 			<label for="cpf">CPF:</label><br/>
-				<input type="text" class="form-control" name="cpf" value="05352745139" id="cpf"  /><br/>
+				<input type="text" class="form-control" name="cpf" value="05352745139" id="cpf"/>
 		</div>
 		<div class="form-group">
 			<label for="tel">Telefone:</label><br/>
-				<input type="text" class="form-control" name="telefone" value="6192545754" id="tel"  /><br/>
+				<input type="text" class="form-control" name="telefone" value="6192545754" id="tel"  />
 		</div>
 		<div class="form-group">
-		<label for="email">E-mail:</label><br/>
-			<input type="email" class="form-control" name="email" id="email" value="c40842998720568962162@sandbox.pagseguro.com.br"  /><br/>
+		<label for="email">E-mail:</label><br>
+			<input type="email" class="form-control" name="email" id="email" value="c40842998720568962162@sandbox.pagseguro.com.br"  />
 		</div>
 		<div class="form-group">
 		<label for="pwd">Senha:</label><br/>
-			<input type="password" class="form-control" name="password" value="A951454h6788P344" id="pwd"  /><br/>
+			<input type="password" class="form-control" name="password" value="A951454h6788P344" id="pwd"  />
 		</div>
 	</div>
 	
 	<div class="col-sm-4">
-		<h3>Informações de Endereço</h3>
+		<h3>DADOS DA ENTREGA</h3>
 
-		<strong>CEP:</strong><br/>
-			<input type="text" name="cep" id="cep" maxlength="9"  /><br/><br/>
-
-		<strong>Rua:</strong><br/>
-			<input type="text" name="rua" id="rua"  /><br/><br/>
-
-		<strong>Número:</strong><br/>
-			<input type="text" name="numero" id="numero"  /><br/><br/>
-
-		<strong>Complemento:</strong><br/>
-			<input type="text" name="complemento" /><br/><br/>
-
-		<strong>Bairro:</strong><br/>
-			<input type="text" name="bairro" id="bairro"  /><br/><br/>
-
-		<strong>Cidade:</strong><br/>
-			<input type="text" name="cidade"  id="cidade" /><br/><br/>
-
-		<strong>Estado:</strong><br/>
-			<input type="text" name="estado" id="estado" /><br/><br/>
-
+		<div class="form-group">
+			<label for="cep">CEP:</label><br/>
+				<input type="text" class="form-control" name="cep" id="cep" maxlength="9"  />
+		</div>
+		<div class="form-group">
+			<label for="rua">Rua:</label><br/>
+				<input type="text" class="form-control" name="rua" id="rua"  />
+		</div>	
+		<div class="form-group">
+		<label for="numero">Número:</label><br/>
+			<input type="text" class="form-control" name="numero" id="numero"  />
+		</div>
+		<div class="form-group">
+			<label>Complemento:</label><br/>
+				<input type="text" class="form-control" name="complemento" />
+		</div>
+		<div class="form-group">
+			<label for="bairro">Bairro:</label><br/>
+				<input type="text" class="form-control" name="bairro" id="bairro"  />
+		</div>
+		<div class="form-group">
+			<label for="cidade">Cidade:</label><br/>
+				<input type="text" class="form-control" name="cidade"  id="cidade" />
+		</div>
+		<div class="form-group">
+			<label for="estado">Estado:</label><br/>
+				<input type="text" class="form-control" name="estado" id="estado" />
+		</div>	
 	</div>	
 	
 	<div class="col-sm-4">
-		<h3>Informações de Pagamento</h3>
 
-		<strong>Titular do cartão:</strong><br/>
-			<input type="text" name="cartao_titular"  /><br/><br/>
+		<h3>DADOS DO PAGAMENTO</h3>
 
-		<strong>CPF do Titular do cartão:</strong><br/>
-			<input type="text" name="cartao_cpf"  /><br/><br/>
-
-		<strong>Número do cartão:</strong><br/>
-			<input type="text" name="cartao_numero" /><br/><br/>
-
-		<strong>Código de Segurança:</strong><br/>
-			<input type="text" name="cartao_cvv" maxlength="3"  /><br/><br/>
-
-		<strong>Validade:</strong><br/>
-			<select name="cartao_mes">
-				<?php for($q=1;$q<=12;$q++): ?>
-				<option><?php echo ($q<10)?'0'.$q:$q; ?></option>
-				<?php endfor; ?>
-			</select>
-			<select name="cartao_ano">
-				<?php $ano = intval(date('Y')); ?>
-				<?php for($q=$ano;$q<=($ano+20);$q++): ?>
-				<option><?php echo $q; ?></option>
-				<?php endfor; ?>
-			</select><br/><br/>
-
-		<strong>Parcelas:</strong><br/>
-			<select name="parc"></select><br/><br/>
-
+		<div class="form-group">
+			<label>Titular do cartão:</label><br/>
+				<input type="text" class="form-control" name="cartao_titular"  />
+		</div>
+		<div class="form-group">
+			<label>CPF do Titular do cartão:</label><br/>
+				<input type="text" class="form-control" name="cartao_cpf"  />
+		</div>
+		<div class="form-group">
+			<label>Número do cartão:</label><br/>
+				<input type="text" class="form-control" name="cartao_numero" />
+		</div>		
+		<div class="form-group">
+			<label>Código de Segurança:</label><br/>
+				<input type="text" class="form-control" name="cartao_cvv" maxlength="3"  />
+		</div>
+		<div class="form-group">
+			<label>Validade:</label><br/>
+				<select name="cartao_mes" class="form-control" style="width: 30%;">
+					<?php for($q=1;$q<=12;$q++): ?>
+					<option><?php echo ($q<10)?'0'.$q:$q; ?></option>
+					<?php endfor; ?>
+				</select>
+				<select name="cartao_ano" class="form-control" style="width: 30%;">
+					<?php $ano = intval(date('Y')); ?>
+					<?php for($q=$ano;$q<=($ano+20);$q++): ?>
+					<option><?php echo $q; ?></option>
+					<?php endfor; ?>
+				</select>
+		</div>
+		<div class="form-group">
+			<label>Parcelas:</label>
+				<select name="parc" class="form-control"></select><br/>
+		</div>
 		<input type="hidden" name="total" value="<?php echo $total; ?>" />
 
 		<button class="button efetuarCompra">Efetuar Compra</button>
